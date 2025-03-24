9d1950ff3c6608961928be54bfa51f9f77699f41
zips314 <zpaulshipp@gmail.com>
2025-03-23 23:20:25 -0500

    Polished the buttons/cards up and added their images

--- obj/Release/net9.0/CapstoneTeam11.AssemblyInfo.cs
+++ obj/Release/net9.0/CapstoneTeam11.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapstoneTeam11")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925598fb08bbb0d362d9ab89d7891fe7a46f0855")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapstoneTeam11")]
