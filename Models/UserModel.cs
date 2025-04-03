d3b33c1dd384b15410f72b4556e571fca4066b4c
Erin Shawhan <111887486+erinshawhan@users.noreply.github.com>
2025-04-03 15:50:05 -0500

    set up user model - for real

---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+using MongoDB.Bson;
+
+namespace CapstoneTeam11.Models;
+
+public class UserModel
+{
+    private int Id { get; set; }
+
+    private AccessLevel { get; set; }
+
+    private string Email { get; set; }
+
+    private string Password { get; set; }
+
+    private string Name { get; set; }
+}
