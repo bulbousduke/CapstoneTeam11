a9c53cfa4774d38c98a95311fcacbecf28211ec9
lana <lana@mwc-153154.dhcp.missouri.edu>
2025-04-30 20:11:49 -0700

    Help page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,43 +5,48 @@ using MongoDB.Driver;
 using CapstoneTeam11.Services;
 using System.Threading.Tasks;
 
-namespace CapstoneTeam11.Controllers;
-
-public class HomeController : Controller
+namespace CapstoneTeam11.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly IMongoCollection<User> _users;
-    private readonly UserService _userService;
-    
-    public HomeController(ILogger<HomeController> logger, IMongoClient mongoClient, UserService userService)
+    public class HomeController : Controller
     {
-        _logger = logger;
-        _userService = userService;
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMongoCollection<User> _users;
+        private readonly UserService _userService;
+        
+        public HomeController(ILogger<HomeController> logger, UserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
 
-        // define database and collection variables
-        var database = mongoClient.GetDatabase("TICKLR");
-        _users = database.GetCollection<User>("users");
-    }
+            // define database and collection variables
+        }
 
-    public IActionResult Index()
-    {
-        return View();
+        public IActionResult Index()
+        {
+            return View();
 
-        // TEST: displaying data from the database
-        // var allUsers = _users.Find(_ => true).ToList(); // get all users
-        // return View(allUsers); // pass data to the view
-    }
+            // TEST: displaying data from the database
+            // var allUsers = _users.Find(_ => true).ToList(); // get all users
+            // return View(allUsers); // pass data to the view
+        }
 
-    public async Task<IActionResult> Privacy()
-    {
-        var allUsers = await _userService.GetAllUsers();
-        return View(allUsers); // pass data to the view
-        // return View();
-    }
+        public async Task<IActionResult> Privacy()
+        {
+            var allUsers = await _userService.GetAllUsers();
+            return View(allUsers); // pass data to the view
+            // return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // Correct placement of Help method inside HomeController class
+        public IActionResult Help()
+        {
+            return View(); // returns the Help view
+        }
     }
 }
