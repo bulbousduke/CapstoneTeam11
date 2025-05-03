62c7f906ce237ac08f767920cc262fc73e8d55d8
Erin Shawhan <ekshawhan@gmail.com>
2025-05-03 16:57:21 -0500

    refactor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,52 +9,36 @@ using Microsoft.AspNetCore.Authorization;
 namespace CapstoneTeam11.Controllers;
 
 [Authorize]
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger, UserService userService) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly IMongoCollection<User> _users;
-    private readonly UserService _userService;
-    
-    public HomeController(ILogger<HomeController> logger, IMongoClient mongoClient, UserService userService)
-    {
-        _logger = logger;
-        _userService = userService;
+    private readonly ILogger<HomeController> _logger = logger;
+    private readonly UserService _userService = userService;
 
-            // define database and collection variables
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-
-            // TEST: displaying data from the database
-            // var allUsers = _users.Find(_ => true).ToList(); // get all users
-            // return View(allUsers); // pass data to the view
-        }
-
-        public async Task<IActionResult> Privacy()
-        {
-            var allUsers = await _userService.GetAllUsers();
-            return View(allUsers); // pass data to the view
-            // return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        // Correct placement of Help method inside HomeController class
-        public IActionResult Help()
-        {
-            return View(); // returns the Help view
-        }
-    
-
-    public IActionResult Login()
+    public IActionResult Index()
     {
         return View();
+
+        // TEST: displaying data from the database
+        // var allUsers = _users.Find(_ => true).ToList(); // get all users
+        // return View(allUsers); // pass data to the view
     }
 
+    public async Task<IActionResult> Privacy()
+    {
+        var allUsers = await _userService.GetAllUsers();
+        return View(allUsers); // pass data to the view
+        // return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    // Correct placement of Help method inside HomeController class
+    public IActionResult Help()
+    {
+        return View(); // returns the Help view
+    }
 }
