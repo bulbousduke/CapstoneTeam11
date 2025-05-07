7fc5e4f2f7b92aa193aa3066ee734063963c4d0c
lana <lana@mwc-152239.dhcp.missouri.edu>
2025-05-06 22:28:25 -0700

    Add password strength validation on registration

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
+
 
 namespace CapstoneTeam11.Controllers
 {
@@ -30,6 +32,17 @@ namespace CapstoneTeam11.Controllers
             if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
             {
                 ViewBag.Error = "All fields are required.";
+                return View();
+            }
+            if (password.Length < 8 || password.Length > 64)
+            {
+                ViewBag.Error = "Password must be between 8 and 64 characters.";
+                return View();
+            }
+                
+            if (!Regex.IsMatch(password, @"\d"))
+            {
+                ViewBag.Error = "Password must include at least one number.";
                 return View();
             }
 
